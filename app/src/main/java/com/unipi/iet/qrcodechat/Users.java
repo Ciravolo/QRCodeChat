72b2b2cd1fc15d99396ae03d14847b463317b03c
Pietro Ciravolo <pietrociravolo@gmail.com>
2018-04-28 17:11:16 +0200

    notification code added, but not working

--- app/src/main/java/com/unipi/iet/qrcodechat/Users.java
+++ app/src/main/java/com/unipi/iet/qrcodechat/Users.java
@@ -4,7 +4,9 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -41,8 +43,8 @@ public class Users extends AppCompatActivity {
     ArrayList<String> al = new ArrayList<>();
     int totalUsers = 0;
     ProgressDialog pd;
-    Firebase reference1;
-
+    //Lista di riferimenti al database
+    ArrayList<Firebase> references = new ArrayList<>();
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -58,38 +60,67 @@ public class Users extends AppCompatActivity {
         setContentView(R.layout.activity_users);
         usersList = (ListView)findViewById(R.id.usersList);
         noUsersText = (TextView)findViewById(R.id.noUsersText);
-
         pd = new ProgressDialog(Users.this);
         pd.setMessage("Loading...");
         pd.show();
 
         String url = "https://qrcodechat-ca31a.firebaseio.com/exchanges/"+UserDetails.username+".json";
 
-        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>(){
-            @Override
-            public void onResponse(String s) {
-                doOnSuccess(s);
-            }
-        },new Response.ErrorListener(){
-            @Override
-            public void onErrorResponse(VolleyError volleyError) {
-                System.out.println("" + volleyError);
-            }
-        });
+        StringRequest request = new StringRequest(Request.Method.GET, url, s -> doOnSuccess(s), volleyError -> System.out.println("" + volleyError));
 
         RequestQueue rQueue = Volley.newRequestQueue(Users.this);
         rQueue.add(request);
 
-        usersList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                UserDetails.chatWith = al.get(position);
-                Log.i("chatWith:",UserDetails.chatWith);
-                startActivity(new Intent(Users.this, Chat.class));
-            }
+        //Ciclo for che attiva le notifiche
+        for(int i = 0; i != references.size(); ++i) { //Per ogni riferimento
+            String temp = al.get(i);
+            references.get(i).addChildEventListener(new ChildEventListener() { //Si vede se vengono aggiunti nuovi nodi
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) { //Quando un nodo viene aggiunto
+                    Map map = dataSnapshot.getValue(Map.class);
+                    String message = map.get("message").toString(); //Prendiamo messaggio e username
+                    String userName = map.get("user").toString();
+
+                    if (!userName.equals(UserDetails.username)) { //Se l'username non è uguale al mio
+                        UserDetails.chatWith = temp;                //Dico che voglio parlare con questo username
+                        Intent notificationIntent = new Intent(getApplicationContext(), Chat.class); //Imposto un intent per aprire la chat con questo utente
+                        PendingIntent contentIntent = PendingIntent.getActivity(getApplicationContext(), 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                        Notification.Builder builder = new Notification.Builder(getApplicationContext()) //Costruisco la notifica
+                                .setSmallIcon(R.drawable.icon)
+                                .setContentIntent(contentIntent)
+                                .setContentTitle("Notifications from " + UserDetails.chatWith)
+                                .setContentText(message);
+                        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE); //Creo un gestore della notifica
+                        manager.notify(0, builder.build());
+                    }
+                }
+
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onCancelled(FirebaseError firebaseError) {
+
+                }
+            });
+        }
+        usersList.setOnItemClickListener((parent, view, position, id) -> {
+            UserDetails.chatWith = al.get(position);
+            Log.i("chatWith:",UserDetails.chatWith);
+            startActivity(new Intent(Users.this, Chat.class));
         });
-
-
     }
 
     public void doOnSuccess(String s){
@@ -101,9 +132,9 @@ public class Users extends AppCompatActivity {
 
             while(i.hasNext()){
                 key = i.next().toString();
-
                 if(!key.equals(UserDetails.username)) {
                     al.add(key);
+                    references.add(new Firebase("https://qrcodechat-ca31a.firebaseio.com/messages/" + UserDetails.username + "_" + key)); //Aggiungo un riferimento per questo utente
                 }
 
                 totalUsers++;
@@ -120,7 +151,7 @@ public class Users extends AppCompatActivity {
         else{
             noUsersText.setVisibility(View.GONE);
             usersList.setVisibility(View.VISIBLE);
-            usersList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, al));
+            usersList.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, al));
         }
 
         pd.dismiss();
